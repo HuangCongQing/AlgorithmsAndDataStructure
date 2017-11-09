d13963f44df785ad8e9ff3a0b20cb38765084579
HuangChongqing <1756260160@qq.com>
2017-11-09 23:49:57 +0800

    简单排序

--- JavaData/src/ch02/BubbleSort.java
+++ JavaData/src/ch02/BubbleSort.java
@@ -7,7 +7,7 @@ public class BubbleSort {
 		for(int i = 0; i < arr.length - 1; i++) {
 			for(int j = arr.length - 1; j > i; j--) {
 				if(arr[j] < arr[j - 1]) {
-					//½øÐÐ½»»»
+					//è¿›è¡Œäº¤æ¢
 					tmp = arr[j];
 					arr[j] = arr[j - 1];
 					arr[j - 1] = tmp;
